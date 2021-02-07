3f35c508b06881ae46687736119bc4cf1c5c8c7d
d <d>
2021-02-07 13:26:11 +0300

    4

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
-import os
-import flask
 import telebot
-from telebot import types
-from config import *
+import os
+from flask import Flask, request
 
-server = flask.Flask(__name__)
+
+TOKEN = '1508006515:AAFRz-eNDwas7qdgDb_k6DQXRNheIHjt06k'
 bot = telebot.TeleBot(TOKEN)
 
-@server.route('/' + TOKEN, methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(
-        flask.request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route('/', methods=["GET"])
-def index():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-    return "Hello from Heroku!", 200
-
-
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+def handle_text(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True,False)
+    user_markup.row('/start','/info')
+    start_text = str('Привет, '+message.from_user.first_name+'!\nЯ бот на Heroku.')
+    bot.send_message(chat_id=1154965888, text=start_text, parse_mode='Markdown')
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://test-new-new.herokuapp.com/' + tokenBot.TOKEN)
+    return "!", 200
 
 
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8443)))
+if __name__ == '__main__':
+    server.debug = True
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
