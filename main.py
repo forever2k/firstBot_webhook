03181f4346e87873d982d3ecd11a999d85a4a41b
d <d>
2021-02-07 22:24:36 +0300

    4

--- main.py
+++ main.py
@@ -14,32 +14,32 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-try:
-    db = mysql.connector.connect(
-        host="eu-cdbr-west-03.cleardb.net",
-        user="bace46a62efed4",
-        passwd="d2459be9",
-        port="3306",
-        database="heroku_504b63985a38758")
-    print(db)
-
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-    sys.exit()
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-    sys.exit()
-  else:
-    print(err)
-    sys.exit()
-
-
-cursor = db.cursor()
-
-cursor.execute("CREATE DATABASE youtube")
-
-cursor.execute("SHOW DATABASES")
+# try:
+#     db = mysql.connector.connect(
+#         host="eu-cdbr-west-03.cleardb.net",
+#         user="bace46a62efed4",
+#         passwd="d2459be9",
+#         port="3306",
+#         database="heroku_504b63985a38758")
+#     print(db)
+#
+# except mysql.connector.Error as err:
+#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#     print("Something is wrong with your user name or password")
+#     sys.exit()
+#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#     print("Database does not exist")
+#     sys.exit()
+#   else:
+#     print(err)
+#     sys.exit()
+#
+#
+# cursor = db.cursor()
+#
+# cursor.execute("CREATE DATABASE youtube")
+#
+# cursor.execute("SHOW DATABASES")
 
 
 
