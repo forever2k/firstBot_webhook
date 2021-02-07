0e77d2a1de26c8098caa9e9710af09245c993ed2
d <d>
2021-02-07 12:28:59 +0300

    4

--- main.py
+++ main.py
@@ -1,105 +1,36 @@
+import os
+
+from flask import Flask, request
+
 import telebot
-from telebot import types
 
-# @BtfNeDetBot
-
-bot = telebot.TeleBot("1508006515:AAFRz-eNDwas7qdgDb_k6DQXRNheIHjt06k")
-
-user_data = {}
-
-class User:
-    def __init__(self, fname):
-        self.fname = fname
-        self.lname = ''
-
+TOKEN = '1508006515:AAFRz-eNDwas7qdgDb_k6DQXRNheIHjt06k'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.send_message(message.from_user.id, "Приветики")
-    # bot.reply_to(message, "Приветики")
-    keyboard = types.InlineKeyboardMarkup()
-    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
-    keyboard.add(key_yes)
-    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
-    keyboard.add(key_no)
-    question = 'Ты хочешь задать вопрос?'
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard) \
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    if call.data == "yes":
-        bot.send_message(call.message.chat.id, "Приятно познакомиться! Теперь запишу в БД!")
-    elif call.data == "no":
-        bot.send_message(call.message.chat.id, "Тогда бай бай!")
-        # bot.register_next_step_handler(call.message, reg_name))
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-#     if message.text == 'Привет':
-#         bot.reply_to(message, 'Привет создатель бота!')
-#     elif message.text == 'hi':
-#         bot.reply_to(message, 'Hi again! The bot creator!')
-#     elif message.text == '/reg':
-#
-#         bot.send_message(message.from_user.id, "Привет! Давай познакомимся! Как тебя зовут?")
-#         bot.register_next_step_handler(message, reg_name)
-#
-#
-# def reg_name(message):
-#     global name
-#     name = message.text
-#     bot.send_message(message.from_user.id, "Какая у вас фамилия?")
-#     bot.register_next_step_handler(message, reg_surname)
-#
-#
-# def reg_surname(message):
-#     global surname
-#     surname = message.text
-#     bot.send_message(message.from_user.id, "Сколько вам лет?")
-#     bot.register_next_step_handler(message, reg_age)
-#
-#
-# def reg_age(message):
-#     global age
-#
-#     while age == 0:
-#         age = int(message.text)
-#
-#     if message.text == int:
-#         age = message.text
-#     else:
-#         bot.send_message(message.from_user.id, "Вводите цифрами!")
-#     # age = message.text
-#     # while age == 0:
-#     #     try:
-#     #         age = int(message.text)
-#     #     except Exception:
-#     #         bot.send_message(message.from_user.id, "Вводите цифрами!")
-#
-#     keyboard = types.InlineKeyboardMarkup()
-#     key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
-#     keyboard.add(key_yes)
-#     key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
-#     keyboard.add(key_no)
-#     question = 'Тебе ' + str(age) + ' лет? И тебя зовут: ' + name + ' ' + surname + '?'
-#     bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
-#
-# #
-# @bot.callback_query_handler(func=lambda call: True)
-# def callback_worker(call):
-#     if call.data == "yes":
-#         bot.send_message(call.message.chat.id, "Приятно познакомиться! Теперь запишу в БД!")
-#     elif call.data == "no":
-#         bot.send_message(call.message.chat.id, "Попробуем еще раз!")
-#         bot.send_message(call.message.chat.id, "Привет! Давай познакомимся! Как тебя зовут?")
-#         bot.register_next_step_handler(call.message, reg_name)
-#
-#
-# bot.polling()
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://myfirst-bot-webhook2.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
