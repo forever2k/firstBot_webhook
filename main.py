3c21215f9011b24ea989504c00f4653dde5e72ed
d <d>
2021-02-08 22:40:05 +0300

    4

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import telebot
 import os
 from flask import Flask, request
 from config import *
-
+import mysql.connector
 
 bot = telebot.TeleBot(TOKEN)
 
-
 server = Flask(__name__)
+
+
+db = mysql.connector.connect(
+        host="r6ze0q02l4me77k3.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+        user="l3q6pdom2dmo7xli",
+        passwd="x5eaeixe429vuh0g",
+        port="3306",
+        database="eyefvtclr0ydnawm")
+
+print(db)
 
 
 @bot.message_handler(commands=['start'])
